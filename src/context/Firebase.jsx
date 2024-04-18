a36b58c8e16de9f808b141c48ad4a6ec04eae8ab
Yuvraj <devyuvraj883@gmail.com>
2024-04-19 00:57:58 +0530

    Initialized firebase set up

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,7 +1,19 @@
 import {useContext, createContext, useState} from 'react';
+import {initializeApp} from 'firebase/app';
 
 export const FirebaseContext = createContext(null);
 // export const useFirebase =  useContext(firebaseContext);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAn46s0K-VQ0Ko9nCvb9T8R8bvLZvrdOYw",
+  authDomain: "placement-cell-400d5.firebaseapp.com",
+  projectId: "placement-cell-400d5",
+  storageBucket: "placement-cell-400d5.appspot.com",
+  messagingSenderId: "204794261592",
+  appId: "1:204794261592:web:281b9447cc20fe63a89a2d",
+  measurementId: "G-9X9QZ3QBD5"
+};
+const firebaseApp = initializeApp(firebaseConfig); 
 
 export const FirebaseProvider = (props)=> {
   const [name, setName] = useState("Yuvraj");
