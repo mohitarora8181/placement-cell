d027106f335e07b775175c2445373175601dbe02
Yuvraj <devyuvraj883@gmail.com>
2024-04-19 02:07:33 +0530

    Added sign-in and sign-up routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { FirebaseContext } from './context/Firebase'
 import { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   const firebase = useContext(FirebaseContext)
@@ -11,6 +13,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/profile' element = {<h1>Profile</h1>}/>
+        <Route path='/sign-in' element = {<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+
       </Routes>
     </>
   )
