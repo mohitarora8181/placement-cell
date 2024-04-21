cf5e7180b2c14767f7e674ebe7b131a3c1a9fd5c
Yuvraj <devyuvraj883@gmail.com>
2024-04-22 02:23:08 +0530

    Added fields for sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -22,10 +22,16 @@ const SignUp = () => {
   const firebase = useContext(FirebaseContext);
 
   const navigate = useNavigate()
+  const [name, setName] = useState('');
+  const [degree, setDegree] = useState('');
+  const [course, setCourse] = useState('');
+  const [dob, setDob] = useState(null);
+  const [cgpa, setCGPA] = useState('');
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+
 
   const handleSignUp = async(e) => {
     e.preventDefault();
@@ -54,6 +60,63 @@ const SignUp = () => {
           style={{ width: '100%', marginTop: '8px' }}
           onSubmit={handleSignUp}
         >
+            <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='name'
+            label='Name'
+            type='text'
+            id='name'
+            autoComplete='current-password'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+<TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='dob'
+            label='Date of Birth'
+            type='date'
+            id='dob'
+            // autoComplete='current-password'
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+          />
+
+<TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='degree'
+            label='degree'
+            type='text'
+            id='degree'
+            // autoComplete='current-password'
+            value={degree}
+            onChange={(e) => setDegree(e.target.value)}
+          />
+
+<TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='course'
+            label='course'
+            type='type'
+            id='text'
+            // autoComplete='current-password'
+            value={course}
+            onChange={(e) => setCourse(e.target.value)}
+          />
+
+
           <TextField
             variant='outlined'
             margin='normal'
@@ -80,10 +143,7 @@ const SignUp = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
+
           <Button
             sx={{ background: '#ea580c' }}
             type='submit'
@@ -94,18 +154,7 @@ const SignUp = () => {
           >
             Sign Up
           </Button>
-          {/* <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid> */}
+
           {error && (
             <Typography
               variant='body2'
