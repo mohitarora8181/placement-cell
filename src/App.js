64422345c6e222b122cc874eb4bd2d9dec11abc1
Yuvraj <devyuvraj883@gmail.com>
2024-04-23 04:28:17 +0530

    Added route to dashboard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { FirebaseContext } from './context/Firebase'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import { useNavigate } from 'react-router-dom'
+import AdminPage from './pages/AdminPage'
 
 function App() {
   const firebase = useContext(FirebaseContext);
   const navigate = useNavigate();
-  if(!firebase.loggedIn){
-    navigate('sign-in');
-  }
-  else{
-    navigate('/');
-  }
+  useEffect(()=>{
+    // if(!firebase.loggedIn){
+    //   navigate('sign-in');
+    // }
 
+
+  },[firebase.loggedIn, navigate]);
   return (
     <>
       <Routes>
@@ -25,6 +26,7 @@ function App() {
         <Route path='/sign-in' element = {<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
         <Route path="/user-profile" element={<Profile/>}/>
+        <Route path="/admin" element={<AdminPage/>}/>
 
       </Routes>
     </>
