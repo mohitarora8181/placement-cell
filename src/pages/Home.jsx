267e57c34444e467e16885f91fcb85a6efca4526
Yuvraj <devyuvraj883@gmail.com>
2024-04-23 05:24:16 +0530

    Added JobPosting component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import JobPostings from "../components/JobPostings";
 import Navbar from "../components/Navbar";
 
 const Home = ()=>{
   return(
     <>
    <Navbar/>
+   <JobPostings/>
 
     </>
   )
