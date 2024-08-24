3324375f8e003f066fe59913abb3d07d2734e689
Yuvraj <devyuvraj883@gmail.com>
2024-08-25 00:19:59 +0530

    Added routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,20 +4,21 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import AdminPage from './pages/AdminPage'
-import AddJob from './pages/Jobadd'
-//import ProtectedRoute from './components/ProtectedRoute'; 
+import AddJob from './pages/AddJob'
+// import PostJob from './pages/PostJob'
+//import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App() {
- 
+
   return (
     <>
       <Routes>
       <Route path='/' element={<SignUp />} />
       <Route path='/profile' element={<Profile />} />
       <Route path="/home/user-profile" element={<Profile />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/job" element={<AddJob />} />
+      <Route path="/admin/post-job" element={<AddJob/>} />
+      <Route path="/admin" element={<AdminPage/>} />
 
       <Route path='/home' element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
