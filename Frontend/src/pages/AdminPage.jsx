ed4bf7a652c8d2a1ce26dbec47ef06f4d4cfd1d5
Yuvraj <devyuvraj883@gmail.com>
2024-08-25 15:05:45 +0530

    Updated to recieve props fromt he child component

--- Frontend/src/pages/AdminPage.jsx
+++ Frontend/src/pages/AdminPage.jsx
@@ -1,11 +1,28 @@
 import React, { useState, useContext } from 'react';
 import AdminNav from '../components/AdminNav';
+import AdminSidebar from '../components/AdminSidebar';
+import AdminStudents from '../components/AdminStudents';
 
 
 const AdminPage = () => {
+  const [display, setDisplay] = useState('student');
+
+  const getDisplayValue = (data)=>{
+    setDisplay(data);
+  }
+
  return(
   <>
   <AdminNav/>
+  <div className='flex flex-row h-screen '>
+    <AdminSidebar onData={getDisplayValue}/>
+    { display==='student' &&
+      <AdminStudents />
+
+    }
+
+
+  </div>
   </>
  )
 };
