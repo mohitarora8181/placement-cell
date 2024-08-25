a5a2b44ed05fda65b136bff13122ba8a5d2d2f50
Yuvraj <devyuvraj883@gmail.com>
2024-08-25 15:20:32 +0530

    Updated style

--- Frontend/src/components/AdminSidebar.jsx
+++ Frontend/src/components/AdminSidebar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-
+import { PiStudentBold } from "react-icons/pi";
+import { CgOrganisation } from "react-icons/cg";
 const AdminSidebar = ({onData})=>{
   const [currentVal, setCurrentVal] = useState('student');
 
@@ -13,17 +14,20 @@ const AdminSidebar = ({onData})=>{
 
 
   useEffect(()=>{
-    onData(currentVal); 
+    onData(currentVal);
   },[currentVal]);
 
   return(
     <>
-      <nav className="h-full w-[30%] bg-[#099934] text-white font-semibold pt-4 text-md">
+      <nav className="h-full w-[20%] bg-[#099934] text-white font-semibold pt-4 text-md shadow-lg mr-4">
         <ul className="flex flex-col p-4">
-          <li className="cursor-pointer " onClick={setToStudent}>Students
+          <li className="cursor-pointer mb-2" onClick={setToStudent}>
+            <span className="flex justify-between pb-2 items-center">Students <PiStudentBold className="text-3xl" /></span>
             <hr/>
           </li>
-          <li className="cursor-pointer " onClick={setToCompany}>Companies
+          <li className="cursor-pointer mb-2" onClick={setToCompany}>
+          <span className="flex justify-between pb-2 items-center">Companies <CgOrganisation className="text-3xl" /></span>
+
             <hr></hr>
           </li>
         </ul>
