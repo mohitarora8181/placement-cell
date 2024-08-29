fef69c5ddd1ba75cd0f913705fb2206a4161f813
Yuvraj <devyuvraj883@gmail.com>
2024-08-29 20:08:51 +0530

    Updated style

--- Frontend/src/components/AdminSidebar.jsx
+++ Frontend/src/components/AdminSidebar.jsx
@@ -4,7 +4,9 @@ import { CgOrganisation } from "react-icons/cg";
 import UserCard from "./UserCard";
 import axios from "axios";
 import JobData from "./JobData";
+import { FaPlus } from "react-icons/fa";
 import JobCard from "./JobCard";
+import { Link } from "react-router-dom";
 const AdminSidebar = ({onData})=>{
   const [currentVal, setCurrentVal] = useState('student');
   const [users, setUsers] = useState([]);
@@ -44,7 +46,7 @@ const AdminSidebar = ({onData})=>{
 
   return(
     <>
-      <nav className="h-full w-[20%] bg-[#099934] text-white font-semibold pt-4 text-md shadow-lg mr-4">
+      <nav className="hidden md:flex flex-col h-full w-[20%] bg-[#099934] text-white font-semibold pt-4 text-md shadow-lg mr-4">
         <ul className="flex flex-col p-4">
           <li className="cursor-pointer mb-2" onClick={setToStudent}>
             <span className="flex justify-between pb-2 items-center">Students <PiStudentBold className="text-3xl" /></span>
@@ -55,6 +57,13 @@ const AdminSidebar = ({onData})=>{
 
             <hr></hr>
           </li>
+          <Link to='/post-job'>
+          <li className="cursor-pointer mb-2" onClick={setToCompany}>
+          <span className="flex justify-between pb-2 items-center">Post Jobs<FaPlus className="text-3xl" /></span>
+
+            <hr></hr>
+          </li>
+          </Link>
         </ul>
       </nav>
       <div className="flex flex-wrap">
