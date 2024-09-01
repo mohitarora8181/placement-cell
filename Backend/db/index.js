73e96ccf1d1e5d79849f57fbac0fd92b99d59ce6
aditya <adityaray947@gmail.com>
2024-09-02 01:09:27 +0530

    testing 1

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../Constant.js";
 import dotenv from "dotenv"
-
+const URI="mongodb+srv://adityaray947:adityaray125@cluster0.kldwlex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 dotenv.config({
     path:'./.env'
 })
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+        const connectionInstance = await mongoose.connect(URI, {
            
             useNewUrlParser: true,
             useUnifiedTopology: true,
