8d3d36e999f3bc567e12f806ff931ce3559b3a32
aditya <adityaray947@gmail.com>
2024-09-02 00:45:51 +0530

    MonogoDB

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../Constant.js";
 
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+           
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
+        console.log("MONGODB connection FAILED", error);
+        process.exit(1);
     }
-}
+};
 export default connectDB
