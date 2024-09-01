bf97b582fb344e55abb508060fbeaed7cecff865
aditya <adityaray947@gmail.com>
2024-09-02 01:17:22 +0530

    test 2

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../Constant.js";
 import dotenv from "dotenv"
-const URI="mongodb+srv://adityaray947:adityaray125@cluster0.kldwlex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 dotenv.config({
     path:'./.env'
 })
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(URI, {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
            
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED", error);
