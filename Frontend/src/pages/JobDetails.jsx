83b86be3eafe5180ebc1254590f0f5fae3dd95a6
Yuvraj <devyuvraj883@gmail.com>
2024-09-04 10:05:16 +0530

    Fixed style

--- Frontend/src/pages/JobDetails.jsx
+++ Frontend/src/pages/JobDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Typography, Grid } from '@mui/material';
-import AppliedUserCard from '../components/AppliedUserCard.js';
+import AppliedUserCard from '../components/AppliedUserCard.jsx';
 
 const JobDetails = () => {
   const { jobId } = useParams();
@@ -66,7 +66,7 @@ const JobDetails = () => {
       <Typography variant="h5" gutterBottom style={{ marginTop: '20px', fontWeight:'bold' }}>
         Students who applied:
       </Typography>
-      <Grid container spacing={1}>
+      <Grid container spacing={3}>
         {job.applicants && job.applicants.length > 0 ? (
           job.applicants.map(user => (
             <Grid item xs={12} md={6} lg={4} key={user._id}>
