6242581d27d414887d96dc8c4c99be01120fda78
Yuvraj <devyuvraj883@gmail.com>
2024-09-11 20:20:06 +0530

    Added fields to the user model

--- Backend/models/SignupModel.js
+++ Backend/models/SignupModel.js
@@ -2,13 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        index: true
-    },
+    
     email: {
         type: String,
         required: true,
@@ -54,13 +48,8 @@ const userSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    address: {
-        type: String,
-        required: true
-    },
-    school12th: {
-        type: String
-    },
+
+
     tenthPercentage: {
         type: Number
     },
@@ -88,11 +77,42 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    isAdmin: {
-        type: Boolean,
-        default: false
+
+    linkedIn:{
+      type:String,
+      required:true
     },
-    
+    leetCode:{
+      type:String,
+      required:true,
+    },
+    github:{
+      type:String,
+      required:true,
+    },
+    portfolio:{
+      type:String,
+      required:true,
+    },
+
+    backlogsCleared:{
+      type: String,
+      required: true,
+    },
+    state:{
+      type: String,
+      required: true
+    },
+    city:{
+      type:String,
+      required: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false
+  },
+
 }, {
     timestamps: true
 });
@@ -101,12 +121,12 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     console.log('Hashing password:', this.password);
-    this.password = await bcrypt.hash(this.password, 10);  
+    this.password = await bcrypt.hash(this.password, 10);
     console.log('Hashed password:', this.password);
     next();
 });
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    
+
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
